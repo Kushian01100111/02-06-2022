ed547bd6db5a047c689a2c28d8bc678a92554a2e
Pedro Comas <comaspedro@gmail.com>
2022-06-02 18:43:53 -0300

    Nivel 6 Kyu

---
+++ 02-06-2022.js
@@ -0,0 +1,24 @@
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains at least 3 numbers.
+
+// The tests contain some very huge arrays, so think about performance.
+
+function findUniq(arr) {
+    // do magic
+    let Obj = {};
+    arr.forEach(n =>{
+      Obj[n] = ++Obj[n] || 1;
+    })
+    let values = Object.entries(Obj),
+        result ;
+    values.forEach(n =>{
+      if(n[1] === 1){
+        result = Number(n[0]);
+      }
+    });
+  return result;
+  }
+
